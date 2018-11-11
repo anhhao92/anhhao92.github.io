6e9c87e22d436517eda670446422e2dceb2c07f2
Anh Hao Tran <anhhao2809@gmail.com>
2018-11-11 19:41:41 +0700

    Updates

---
+++ precache-manifest.99acd7d9d156048c1e11a4b7a5b42ee3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "82aa9a651207ebb6e588",
+    "url": "/static/css/6.8a8eabd5.chunk.css"
+  },
+  {
+    "revision": "32f5caa5d57888b37a18",
+    "url": "/static/css/main.d0d43c77.chunk.css"
+  },
+  {
+    "revision": "e086cc5aed068a3d9457",
+    "url": "/static/js/runtime~main.e086cc5a.js"
+  },
+  {
+    "revision": "619c4199d29658283740",
+    "url": "/static/js/1.619c4199.chunk.js"
+  },
+  {
+    "revision": "7ecd56db35e543821029",
+    "url": "/static/js/11.7ecd56db.chunk.js"
+  },
+  {
+    "revision": "deff4c1bfe385b1b598d",
+    "url": "/static/js/2.deff4c1b.chunk.js"
+  },
+  {
+    "revision": "77a28055a0ebffe17561",
+    "url": "/static/js/10.77a28055.chunk.js"
+  },
+  {
+    "revision": "7f95c596f4abe01d642c",
+    "url": "/static/js/3.7f95c596.chunk.js"
+  },
+  {
+    "revision": "c3012b1c27d5e8c3b881",
+    "url": "/static/js/4.c3012b1c.chunk.js"
+  },
+  {
+    "revision": "efe5540a15e78a6c8730",
+    "url": "/static/js/9.efe5540a.chunk.js"
+  },
+  {
+    "revision": "648965f2056285795970",
+    "url": "/static/js/5.648965f2.chunk.js"
+  },
+  {
+    "revision": "32f5caa5d57888b37a18",
+    "url": "/static/js/main.32f5caa5.chunk.js"
+  },
+  {
+    "revision": "82aa9a651207ebb6e588",
+    "url": "/static/js/6.82aa9a65.chunk.js"
+  },
+  {
+    "revision": "fe72adff12ef4923d2f3",
+    "url": "/static/js/7.fe72adff.chunk.js"
+  },
+  {
+    "revision": "df6019a602f6efad05a4",
+    "url": "/static/js/8.df6019a6.chunk.js"
+  },
+  {
+    "revision": "efe5540a15e78a6c8730",
+    "url": "/static/css/9.0620a7bd.chunk.css"
+  },
+  {
+    "revision": "df6019a602f6efad05a4",
+    "url": "/static/css/8.1541854b.chunk.css"
+  },
+  {
+    "revision": "648965f2056285795970",
+    "url": "/static/css/5.920d43b0.chunk.css"
+  },
+  {
+    "revision": "7f95c596f4abe01d642c",
+    "url": "/static/css/3.8758d81c.chunk.css"
+  },
+  {
+    "revision": "deff4c1bfe385b1b598d",
+    "url": "/static/css/2.cfc893bf.chunk.css"
+  },
+  {
+    "revision": "7ecd56db35e543821029",
+    "url": "/static/css/11.f8e18c91.chunk.css"
+  },
+  {
+    "revision": "619c4199d29658283740",
+    "url": "/static/css/1.35110425.chunk.css"
+  },
+  {
+    "revision": "67dfc369a8a8cc5f1c948c9d02ceaa14",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18f0ee4e771ff541d0027c49b1c8c674.js"
+  "/precache-manifest.99acd7d9d156048c1e11a4b7a5b42ee3.js"
 );
 
 workbox.clientsClaim();
